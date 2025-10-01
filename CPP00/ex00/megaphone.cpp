bbd1d2415f4f6e248030a48987a304b0ea438bd8
skribisti <nonorabino1211@gmail.com>
2025-10-01 16:40:18 +0200

    CPP00/ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: norabino <norabino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 16:13:46 by norabino          #+#    #+#             */
+/*   Updated: 2025/09/29 16:48:31 by norabino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	int	 i;
+	int	 j;
+
+	i = 1;
+	if (ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (-1);
+	}
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			std::cout << (char)std::toupper(av[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
