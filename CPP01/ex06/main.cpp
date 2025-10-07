3110047b486daf93a5ba9486cd9b7f869290c924
skribisti <nonorabino1211@gmail.com>
2025-10-07 14:37:02 +0200

    CPP01/ex06

---
+++ CPP01/ex06/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: norabino <norabino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 13:49:04 by norabino          #+#    #+#             */
+/*   Updated: 2025/10/07 14:17:36 by norabino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/Harl.hpp"
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cout << "Wrong format." << std::endl;
+		return (1);
+	}
+	std::string level = av[1];
+	if (!av[1] || level.empty() || (level != "DEBUG" && level != "INFO" && level != "WARNING" && level != "ERROR"))
+	{
+		std::cout << "Wrong format for inputed level." << std::endl;
+		return (1);
+	}
+	Harl harl;
+	harl.complain(level);
+	return (0);
+}
