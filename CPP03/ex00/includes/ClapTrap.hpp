50967a1482358ac8b935cc1da40cb65e1851bf4b
skribisti <nonorabino1211@gmail.com>
2025-10-13 16:47:29 +0200

    CPP03/ex00

---
+++ CPP03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: norabino <norabino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 16:04:40 by norabino          #+#    #+#             */
+/*   Updated: 2025/10/13 16:17:14 by norabino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	ClapTrap( void );
+	std::string 	_name;
+	unsigned int			_HP;
+	unsigned int			_EP;
+	unsigned int			_AD;
+
+public:
+	ClapTrap( std::string name );
+	ClapTrap( ClapTrap const & other );
+	~ClapTrap ( void );
+
+	void	attack( const std::string& target );
+	void	takeDamage( unsigned int amount );
+	void	beRepaired( unsigned int amount );
+
+	ClapTrap	&operator=( ClapTrap const &other );
+};
+
+#endif
---
+++ CPP03/ex00/src/ClapTrap.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: norabino <norabino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 16:08:29 by norabino          #+#    #+#             */
+/*   Updated: 2025/10/13 16:47:02 by norabino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ClapTrap.hpp"
+
+ClapTrap::ClapTrap( std::string name ) : _name( name ), _HP( 10 ), _EP( 10 ), _AD( 0 )
+{
+	std::cout << "Constructor called." << std::endl;
+}
+
+ClapTrap::ClapTrap( ClapTrap const &other )
+{
+	*this = other;
+	std::cout << "Copy constructor called." << std::endl;
+}
+
+ClapTrap	& ClapTrap::operator=( ClapTrap const &other )
+{
+	this->_name = other._name;
+	this->_HP = other._HP;
+	this->_EP = other._EP;
+	this->_AD = other._AD;
+	return (*this);
+}
+
+ClapTrap::~ClapTrap ( void )
+{
+}
+
+void	ClapTrap::attack( const std::string& target )
+{
+	if (_HP <= 0 || _EP <= 0) {
+		std::cout << "⛑️   ClapTrap " << _name << " can't do anything." << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _AD << " points of damage!" << std::endl;
+	this->_EP -= 1;
+}
+
+void	ClapTrap::takeDamage( unsigned int amount )
+{
+	if (_HP < amount) {
+		std::cout << "ClapTrap " << _name << " is out of combat." << std::endl;
+		_HP = 0;
+		return ;
+	}
+	this->_HP -= amount;
+}
+
+void	ClapTrap::beRepaired( unsigned int amount )
+{
+	this->_HP += amount;
+	this->_EP --;
+	std::cout << "ClapTrap " << _name << " get repaired  causing " << amount << " energie points gained! " << std::endl;
+}
