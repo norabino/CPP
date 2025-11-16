75a6aa9c81d138f7f0692c19c3874953430a0f28
Noe Rabinovici <norabino@c2r3p2.42perpignan.fr>
2025-11-16 13:51:25 +0100

    CPP03/ex00

--- CPP03/ex00/includes/ClapTrap.hpp
+++ CPP03/ex00/includes/ClapTrap.hpp
@@ -6,12 +6,12 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 16:04:40 by norabino          #+#    #+#             */
-/*   Updated: 2025/11/16 13:23:34 by norabino         ###   ########.fr       */
+/*   Updated: 2025/11/16 13:49:26 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAPTRAP_hpP
-#define CLAPTRAP_hpP
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
 
 #include <iostream>
 
