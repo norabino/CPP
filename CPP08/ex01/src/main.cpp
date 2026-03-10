910e9c55c4f109e7314379acbceaef84d6512523
norabino <nonorabino1211@gmail.com>
2026-03-10 13:04:24 +0100

    CPP08/ex02

--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: norabino <norabino@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 14:37:05 by norabino          #+#    #+#             */
-/*   Updated: 2026/01/30 15:53:43 by norabino         ###   ########.fr       */
+/*   Updated: 2026/03/10 13:01:31 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Span.hpp"
 
-int main()
+/*int main()
 {
 	std::cout << "\033[33m" << "Test vector container\n"<< "\033[0m";
 	Span sp = Span(5);
@@ -82,4 +82,17 @@ int main()
 	{
 		std::cout << e.what() << std::endl;
 	}
+}*/
+
+int main()
+{
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
 }
